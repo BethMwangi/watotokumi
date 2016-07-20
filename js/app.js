bcd51a7cfb1d1251c859dabddef2455eb6df27f2
BethMwangi <wanjikumwangib@gmail.com>
2016-07-20 11:15:08 +0300

    add file structure

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).foundation()
+
+
+
+  // HOME BACKGROUND SLIDESHOW
+  $(function(){
+    jQuery(document).ready(function() {
+    $('#home').backstretch([
+       "img/happy.jpg", 
+       "img/home-bg-slideshow2.jpg",
+        ],  {duration: 2000, fade: 750});
+    });
+  });
+
+});
